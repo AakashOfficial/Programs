8000063e70261dd91fd78237faf96db1c5cba891
Aakash Tyagi <aakashtyagi1996@outlook.com>
2017-08-20 10:22:08 +0530

    Servlet CRUD

---
+++ CRUD/src/com/tyagi/Servlet/RegisterServlet.java
@@ -0,0 +1,24 @@
+package com.tyagi.Servlet;
+/*8036 8037 8038*/
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("ResgiserServlet")
+public class RegisterServlet extends HttpServlet{
+	
+	public void doPost(HttpServletRequest req,HttpServletResponse resp)throws ServletException,IOException {
+		  resp.setContentType("text/html");
+		  PrintWriter out=resp.getWriter();
+		 
+		  String n=req.getParameter("Name");
+		  out.println("Welcome"+n);
+		  
+		  out.close();
+	  }
+
+}
