46779293e9d59b02e93867be8c79656c8d91ea5c
Aakash Tyagi <aakashtyagi1996@outlook.com>
2020-09-11 23:30:45 +0530

    Update Imple.java

--- HeCheck/src/source/Imple.java
+++ HeCheck/src/source/Imple.java
@@ -3,8 +3,6 @@ import java.util.List;
 import getersetter.Data;
 
 public interface Imple {
-
-
   public boolean addComputer(Data c);
   public List<Data> getAllComputer();
   public boolean deleteComputer(int price);
