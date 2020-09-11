3e0a7132d46b3680e98f582119ce76d09884f6ef
Aakash Tyagi <aakashtyagi1996@outlook.com>
2020-09-11 23:34:32 +0530

    Update Data.java

--- HeCheck/src/getersetter/Data.java
+++ HeCheck/src/getersetter/Data.java
@@ -12,27 +12,35 @@ public class Data implements Serializable {
   public int getPrice(){
 	  return price;
   }
+	
   public void setPrice(int price){
 	  this.price=price;
   }
+	
   public String getSName(){
 	  return sName;
   }
+	
   public void setSName(String sName){
 	  this.sName=sName;
   }
+	
   public String getCName(){
 	  return cName;
   }
+	
   public void setCName(String cName){
 	  this.cName=cName;
   }
+	
   public int getquantity(){
 	  return quantity;
   }
+	
   public void setquantity(int quantity){
 	  this.quantity=quantity;
   }
+	
   public String toString(){
 	  return "Computer[price=" + price +",sName=" + sName + ", cName=" + cName +",quantity=" + quantity +"]";
   }
