13f22153d99dd737fe98c8945e2f6ebcf7d6faf7
Aakash Tyagi <aakashtyagi1996@outlook.com>
2020-09-11 23:25:47 +0530

    Update RegisterServlet.java

--- FirstServletApplication/src/Test/RegisterServlet.java
+++ FirstServletApplication/src/Test/RegisterServlet.java
@@ -2,7 +2,6 @@ package Test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
