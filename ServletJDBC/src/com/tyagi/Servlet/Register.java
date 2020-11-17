1e2bbd41d98d86c681e8d5611e669ae19b2a3366
Aakash Tyagi <aakashtyagi1996@outlook.com>
2020-11-17 19:59:48 +0530

    Update Register.java

--- ServletJDBC/src/com/tyagi/Servlet/Register.java
+++ ServletJDBC/src/com/tyagi/Servlet/Register.java
@@ -9,32 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/register")
-	public class Register extends HttpServlet {
+public class Register extends HttpServlet {
 		
-		  
-	 public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException {
-		  resp.setContentType("text/html");
-		  PrintWriter out=resp.getWriter();
-		  String un=req.getParameter("Username");
-		//  String p=req.getParameter("password");
-		 
-		  
-			
-		
-			
-			out.println("welcome"+un);
-			
-			out.println("You are successfully registered");
-						    
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException {
+		resp.setContentType("text/html");
+		PrintWriter out=resp.getWriter();
+		String un=req.getParameter("Username");
+		//  String p=req.getParameter("password"); 
+		out.println("welcome"+un);
+		out.println("You are successfully registered");		    
 
 		 /* out.println("Welcome user \t"+un);
-		  
-		  out.println("Your password is \t"+p);
-		  */
-		  out.close();
+		 out.println("Your password is \t"+p); */
+		 out.close();
 	  }
-	}
-
-	
-
-
+}
